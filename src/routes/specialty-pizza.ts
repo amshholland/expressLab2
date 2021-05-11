becbddfde6cdb34b53efaf117d9591cde478963c
Amber Holland <Amber.S.Creech@gmail.com>
2021-05-11 08:57:33 -0400

    deleted file

--- src/routes/specialty-pizza.ts
+++ src/routes/specialty-pizza.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get( "/specialty-type", ( req, res ) => {
+routes.get( '/specialty-type', ( req, res ) => {
     const type = req.query.type;
     const price = req.query.price;
-    res.render( "specialty-pizza", { type, price } );
+    res.render( 'specialty-pizza', { type, price } );
 } );
 
 export default routes;
